18e227463353304ab1a4082a3c829fa38d5e4b28
keiter85 <k.reynolds3@students.clark.edu>
2019-10-20 19:51:35 -0700

    added

--- scr/MPU_Tester.c
+++ scr/MPU_Tester.c
@@ -1,10 +1,10 @@
 
 
 //#include <stdio.h>
-#include <stdint.h>
+//#include <stdint.h>
 //#include <stdbool.h>
 //#include <unistd.h>
-//#include <signal.h>
+#include <signal.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
