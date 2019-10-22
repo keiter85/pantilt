e8ce8d84bd8d3936951128f22cf10f29451617fe
keiter85 <k.reynolds3@students.clark.edu>
2019-10-21 18:28:57 -0700

    added

--- scr/MPU_Tester.c
+++ scr/MPU_Tester.c
@@ -1,31 +1,32 @@
-
-
 #include <stdio.h>
 //#include <stdint.h>
 //#include <stdbool.h>
 //#include <unistd.h>
 #include <signal.h>
-#include <stdlib.h>
-#include <wiringPi.h>
+//#include <stdlib.h>
+//#include <wiringPi.h>
 //#include <wiringPiI2C.h>
 
-#include "MPU9250.h"
+//#include "MPU9250.h"
 
 
 void intHandler(int dummy) {
     // turn off all channels
     //PCA9685_setAllPWM(fd, addr, _PCA9685_MINVAL, _PCA9685_MINVAL);
-    exit(dummy);
+    //exit(dummy);
 }
 
 int main(void){
-    wiringPiSetup();
+    //wiringPiSetup();
     //wiringPiI2CSetup (68);
-    signal(SIGINT, intHandler);
-    MPU9250_Init();
+    //signal(SIGINT, intHandler);
+    //MPU9250_Init();
     for(;;){
-        MPU9250_READ_ACCEL();
-        printf("accx: %i, accy: %i, accz: %i", accel[0], accel[1], accel[2]);
+        //MPU9250_READ_ACCEL();
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        printf("accx: %i, accy: %i, accz: %i", a, b, c);
         delay(500);
     }
 }
